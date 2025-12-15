bf4c40a1b2ee078a72c7916fbf9bf23b6a58967f
NHORN Lik <126126451+lik09@users.noreply.github.com>
2025-12-15 16:27:23 +0700

    fix file config.js add new file .env.local

--- resources/js/utils/config.js
+++ resources/js/utils/config.js
@@ -1,6 +1,11 @@
-export const config = {
-    base_url_api : "http://localhost:8000/api/",
-    image_path : "http://localhost:8000/storage/",
-    pdf_path :"http://localhost:8000/storage/",
-}
+// export const config = {
+//     base_url_api : "http://localhost:8000/api/",
+//     image_path : "http://localhost:8000/storage/",
+//     pdf_path :"http://localhost:8000/storage/",
+// }
 
+export const config = {
+    base_url_api: import.meta.env.VITE_API_URL,
+    image_path: import.meta.env.VITE_IMAGE_PATH,
+    pdf_path: import.meta.env.VITE_PDF_PATH,
+};
